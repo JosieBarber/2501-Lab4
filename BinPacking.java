f1486842e6ae068793f8dc75983c2134b8c4d7dd
El <and10393@umn.edu>
2024-09-18 19:39:45 -0500

    Update ReadMe and print statements

--- BinPacking.java
+++ BinPacking.java
@@ -1,83 +1,103 @@
+// Josie Barber and El Anderson
 import java.util.Scanner;
 import java.util.Arrays;
 
 
 public class BinPacking {
-
-
     public static void main(String[] args) {
+        //creates new Scanner "sc"
         Scanner sc = new Scanner(System.in);
+        // instruction line
+        // will not take items of size 0
         System.out.println("enter parameters");
+        // first int will be the bin capacity
         int binCapacity = sc.nextInt();
+        // second int will be number of items to follow
         int numItems = sc.nextInt();
 
         int[] nums = new int[numItems];
-
+        // puts rest of ints into array
         for (int i = 0; i < numItems; i++) {
             nums[i] = sc.nextInt(); 
         }
         sc.close();
 
+        // preps the array of numbers to be put into our bin algorithm 
+        //(uses insertion sort because has small input quantities)
         insertionSort(nums);
 
-        // for (int i = 0; i < numItems; i++) {
-        //     System.out.println(nums[i]);
-        // }
+        // calls bin sorting alg on array
+        // called frontLoader bc puts biggest numbers into bins first
+        int [][] bins = frontLoader(3, binCapacity, nums);
 
-    int [][] bins = frontLoader(3, binCapacity, nums);
+        // Return/Print Statements
+        System.out.println("Bin #1 , unused space: " + bins[4][0]);
+        System.out.println(Arrays.toString(bins[0]));
+        System.out.println("Bin #2 , unused space: " + bins[4][1]);
+        System.out.println(Arrays.toString(bins[1]));
+        System.out.println("Bin #3 , unused space: " + bins[4][2]);
+        System.out.println(Arrays.toString(bins[2]));
 
-     System.out.println(Arrays.toString(bins[0]));
-     System.out.println(Arrays.toString(bins[1]));
-     System.out.println(Arrays.toString(bins[2]));
+        System.out.println("-----Unsued Items-----");
+        for (int i = 0; i < bins[3].length; i++) {
+                System.out.println(bins[3][i]);
+            }
 
-    System.out.println("-----Unsued Items----- \n");
-     for (int i = 0; i < bins[3].length; i++) {
-            System.out.println(bins[3][i]);
-        }
+        // Inputs for copy/paste
+        //  20 8 12 4 8 15 9 3 1 10
+        //  20 6 20 20 20 20 20 20
+        //  20 6 10 10 10 10 10 10
     }
 
-    //  20 8 12 4 8 15 9 3 1 10
-    //  20 6 20 20 20 20 20 20
-    //  20 6 10 10 10 10 10 10
-
-
-    //idea one :
-    // sort array/filter
-    // if smaller than bin size put biggest num in first bin,
-    // if under capacity put second biggest numb
-    // continue down list till list ends or bin is full
-    // repeat bins
+    /*
+        FrontLoader :
+        sort array/filter
+        if smaller than bin size put biggest num in first bin,
+        if under capacity put second biggest numb
+        continue down list till list ends or bin is full
+        repeat bins
+    */ 
     public static int[][] frontLoader(int numBoxes, int bSize, int[] nums) {
-        int[][] bins = new int[numBoxes + 1][nums.length];
+        int[][] bins = new int[numBoxes + 2][nums.length];  //one extra box is for unused imports, second is for capacity
         int[] remainingCapacity = new int[numBoxes];
         Arrays.fill(remainingCapacity, bSize);
 
         int unusedItemsIndex = 0;
+        // creates count for unused space in bins
+        // also manages return type issues with remainingCapacity[]
+        bins[4][0] = bSize;
+        bins[4][1] = bSize;
+        bins[4][2] = bSize;
 
+        // traverses each number imported
         for (int i = nums.length-1; i >= 0; i--) {
             boolean placed = false;
 
+            // traverses each of the boxes to find space
             for (int j = 0; j < numBoxes; j++) {
                 if (nums[i] <= remainingCapacity[j]) {
                     bins[j][i] = nums[i];
+                    bins[4][j] -= nums[i]; //updates unused space
                     remainingCapacity[j] -= nums[i];
                     placed = true;
                     break;
                 }
             }
 
+            // adds unused items to unused item bin
             if (!placed) {
                 bins[numBoxes][unusedItemsIndex] = nums[i];
                 unusedItemsIndex++;
             }
         }
 
+        // removes zeros
         for (int i = 0; i < bins.length-1; i++) {
             bins[i] = Arrays.stream(bins[i]).filter(x -> x != 0).toArray();
         }
 
+        // removes zeros from unused item bin
         bins[3] = Arrays.copyOfRange(bins[3], 0, unusedItemsIndex);
-
 
         return bins;
     }
--- README.md
+++ README.md
@@ -1,22 +1,49 @@
-TODO:
-- finish bin sort:
-  - take cases when not just exactly bin size
-  - manage unused imports (check)
-- Show at least two examples for which your algorithm returns an optimal solution and at least one for which it doesn't (i.e. there exists a solution with more less total leftover volume than the one that your algorithm finds). 
-  - Optimal idea 1:
-    - 10 , 7, 10 10 10 2 2 2 2
-    - alg searches for perfect fits first
-  - Optimal 2:
-    - 10 , 6 , 8 8 8 2 2 2
-  - Optimal 3(extra): 
-    - 3 , 9 , 1 1 1 1 1 1 1 1 1 
-  - Un-optimal: 
-    - Bin Size 20 , 9 , 17 17 17 10 10 10 10 10 10 
-      - out alg will take the 17 and put them in the bins and not have enough space for the 10s which would fir perfectly
--  As an extra credit, compute the maximal difference between your solution and the optimal one (in percentage of the total space), show and justify your computations. (ASK PETER)
-- Perform efficiency analysis
-- documentation
+// Josie Barber and El Anderson
 
-Future us issues !!!!! :
-    What if an element is larger than binSize????
-    Handle printing bins when input includes 0
+Notable Import Statements:
+  - Optimal idea 1:
+    - our algorithm sorts optimally when the highest numbers fill the bins completely
+    - Ex: 10 7 10 10 10 2 2 2 2
+      - [10]
+      - [10]
+      - [10]
+      - unused: [2,2,2,2], no leftover space
+  - Optimal idea 2:
+    - our alg performs optimally when the largest and smallest numbers match to fill the bins completely
+    - Ex: 10 , 6 , 8 8 8 2 2 2
+      - [8 , 2]
+      - [8 , 2]
+      - [8 , 2]
+      - unused [none], no leftover space
+  - Not-optimal idea: 
+    - our algorithms favors placing the biggest number first and can miss perfect fits if they are made of numbers smaller than the first three largest
+    - Bin Size 20 , 9 , 17 17 17 10 10 10 10 10 10 
+      - [17]
+      - [17]
+      - [17]
+      - unused [10 , 10 , 10 , 10 , 10, 10], 9 spots leftover
+      - out alg will take the 17 and put them in the bins and not have enough space for the 10s which would fit perfectly
+  - Extra credit for not-optimal input
+    - in this example our algorithm has 9/30 spots leftover, vs the ideal which has 0 spots
+    - Our algorithm leaves 30% of the space unused vs 0% which is for a difference of 30%
+    - this was calculated using: (totalUnusedSpace / (binCapacity*3)) - 0%
+
+- Efficiency Analysis
+  prep:
+  scanner // Theta(1)
+  place items in array // Theta(n)
+  sort alg(insertion) // O(n^2)
+
+  inside frontLoader:
+  create arrays // Theta(1)
+  for loop // runs n times
+    for loop // runs 3 times for a total of 3n
+    if statement // Theta(1*n)
+  for loop // Theta(n)
+  return statement // Theta(1)
+
+  print statements // O(n) , mainly for unused items for loop
+
+  This comes together to make T(n) = Theta(1) + Theta(n) + O(n^2) + Theta(1) + Theta(3n) + Theta(1n) + Theta(n) + Theta(1) + O(n)
+  The big players in this are O(n^2), O(n) and Theta(n), O(n^2) is a dominating runtime compared to linear time
+  This comes together to make T(n) in O(n^2) which is a polynomial runtime!
